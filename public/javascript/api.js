6be9169009bbd3c0065da1cd54fb469ae0398429
Danieli Jaqueline do Nascimento <djaqueline.nascimento@gmail.com>
2023-01-03 14:31:24 -0300

    add remote repository

---
+++ public/javascript/api.js
@@ -0,0 +1,7 @@
+let api_key = "8214fda0f342b0139beddfd5600a6e58"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
